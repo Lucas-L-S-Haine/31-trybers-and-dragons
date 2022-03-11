84ceb9f78cc6f1008f576c138368051922841046
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2022-03-10 21:37:53 -0300

    Classe Archetype implementada.

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,15 @@
+export default class Archetype {
+  private _name: string;
+  special: number;
+  cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this.special = 0;
+    this.cost = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+}
